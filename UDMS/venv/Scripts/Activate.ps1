cba172e1b42f35897f86ddd6bfba14dbd09dd365
Frank Ng <franknigi@hotmail.com>
2019-05-15 20:39:39 +0800

    Fix auto adjustment of footer(#0010)

--- UDMS/venv/Scripts/Activate.ps1
+++ UDMS/venv/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="C:\Users\Enchanted\Desktop\groupproject\venv"
+$env:VIRTUAL_ENV="C:\Users\frank\PycharmProjects\UDMS\venv"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
--- UDMS/venv/Scripts/activate.bat
+++ UDMS/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\Enchanted\Desktop\groupproject\venv"
+set "VIRTUAL_ENV=C:\Users\frank\PycharmProjects\UDMS\venv"
 
 if not defined PROMPT (
     set "PROMPT=$P$G"
